ecfcd302c3971c4785917666f21fce7dd7c85421
João Paulo da Silva <hi@joaopaulo.dev>
2020-02-28 16:46:18 -0300

    Publish

---
+++ precache-manifest.164023c303d12e8faa1a7354a15edefb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a5c3362d79c0f947488ea1f64e69b12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20dc75dd6becefd96071",
+    "url": "/static/js/2.a5ed1e12.chunk.js"
+  },
+  {
+    "revision": "337af8776e89b99720ae0bcbd286cb0d",
+    "url": "/static/js/2.a5ed1e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c7a0c74e6975357e33",
+    "url": "/static/js/main.00f751c3.chunk.js"
+  },
+  {
+    "revision": "df39e9f051d72dfca402",
+    "url": "/static/js/runtime-main.2b08d788.js"
+  }
+]);
